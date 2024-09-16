59ec0f2b68c4e59aaecd31518d41161c136fad2e
ozantalay <ozntalay@hotmail.com>
2024-09-16 03:21:21 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -12,29 +12,30 @@ Bu web sayfası, HTML öğeleri vanilla JS ile DOM'a eklenerek oluşturulmuştur
         4. Kod modüler ve iyi organize edilmiş olmalıdır.
 */
 
-const header = `<header>
-                    <div class="button-container">
-                        <button>Menu</button> 
-                        <button>Hakkında</button> 
-                        <button>Saatler & Lokasyon</button>
-                    </div> 
-                </header>`
-document.body.innerHTML = header
 
-const mainElement = document.createElement('main')
-document.body.appendChild(mainElement)
+function Header(){
+    return (
+        <header>
+        <div class="button-container">
+        <button>Menu</button> 
+        <button>Hakkında</button> 
+        <button>Saatler & Lokasyon</button>
+    </div> 
+        </header>
+    )
+}
 
-const someHeading = document.createElement('h1')
-someHeading.classList.add('logo-container')
-someHeading.textContent = 'Basit Yemekler Restoranı'
-mainElement.appendChild(someHeading)
+function Main(){
+    return (
+        <main>
 
-const someParagraph = document.createElement('p')
-someParagraph.classList.add('info-container')
-someParagraph.textContent = `Basit insanlar için basit yemekler yapıyoruz. Sandviç ister misiniz? Tamamdır. Goralı? Hemen geliyor. Ateşte kavrulmuş kuşkonmaz börekleri ile yerel kaynaklı balla sırlanmış haşlanmış somon yumurtası? Hadi oradan. Caddenin karşısındaki Fantezi Yemek Kafe'yi deneyin.`
-mainElement.appendChild(someParagraph)
+        <h1>
+            Basit Yemekler Restoranı
+        </h1>
 
-const imageContainer = `<div class="image-container">
+        <p className='info-container'>Basit insanlar için basit yemekler yapıyoruz. Sandviç ister misiniz? Tamamdır. Goralı? Hemen geliyor. Ateşte kavrulmuş kuşkonmaz börekleri ile yerel kaynaklı balla sırlanmış haşlanmış somon yumurtası? Hadi oradan. Caddenin karşısındaki Fantezi Yemek Kafe'yi deneyin.</p>
+
+        <div class="image-container">
                             <div class="image-wrapper">
                                 <img class="hot-dog" src="images/hotdogs.jpg"/>
                             </div>
@@ -44,14 +45,30 @@ const imageContainer = `<div class="image-container">
                             <div class="image-wrapper">
                                 <img src="images/hamburger.jpg"/>
                             </div>                                  
-                        </div>`
-mainElement.insertAdjacentHTML('beforeend', imageContainer)
+                        </div>
+        </main>
 
-const footer = `<footer>
+    )
+}
+
+
+
+function Footer(){
+    <footer>
                     <p> 
                         Made by <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank"> Coding Company</a>™.
                     </p>
-                </footer>`
-document.body.innerHTML += footer
+                </footer>
 
-export default header
+}
+function App() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
+
+export default App
